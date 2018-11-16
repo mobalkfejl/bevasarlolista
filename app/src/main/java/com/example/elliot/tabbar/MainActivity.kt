258dc9b7bb31db5e1ca34c391279352c68f8b608
Wagner Robert <wagrob96@gmail.com>
2018-11-16 13:31:16 +0100

    android:id hozzá lett adva

--- app/src/main/java/com/example/elliot/tabbar/MainActivity.kt
+++ app/src/main/java/com/example/elliot/tabbar/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.appcompat.R.id.message
+import android.widget.LinearLayout
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,4 +22,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+    fun clickNote(){
+        val noteLayout : LinearLayout = findViewById(R.id.noteID)
+    }
+
+
 }
